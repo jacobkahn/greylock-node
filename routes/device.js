0eb42fb7b8480c4c9e82a9a9db90ab80f04fc46a
Jacob Kahn <jacobkahn1@gmail.com>
2016-07-09 14:47:14 -0700

    Add create_session route

--- routes/device.js
+++ routes/device.js
@@ -1,13 +1,46 @@
 var express = require('express');
 var hat = require('hat');
+var Device = require('../db/models').device;
 
 var router = express.Router();
 
 router.get('/initialize', function (req, res, next) {
-	res.send({
-		phone_id: hat(),
-		status: 'success',
+	var deviceID = hat();
+	req.models.device.create({
+		deviceID: deviceID,
+	}).then(function (device) {
+		res.send({
+			phone_id: device.deviceID,
+			status: 'success',
+		});
+	}).catch(function (err) {
+		res.send({
+			phone_id: -1,
+			status: 'failure',
+		});
 	});
+});
+
+router.post('/create_session', function (req, res, next) {
+	req.models.device.findOne()
+		.where({
+			deviceID: req.body.phone_id,
+		}).then(function (device) {
+			req.models.session.create({
+				devices: [device.deviceID]
+			}).then(function (session) {
+				res.send({
+					session_id: session.id,
+					status: 'success',
+				});
+			}).catch(function (err) {
+				console.log(err);
+				res.send({
+					session_id: -1,
+					status: 'failure',
+				});
+			});
+		});
 });
 
 module.exports = router;
