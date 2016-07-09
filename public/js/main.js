6a0552447cd795543698d01a9102bba4becc8474
Devesh Dayal <devesh95@gmail.com>
2016-07-09 12:29:43 -0700

    test new screen locking

--- public/js/main.js
+++ public/js/main.js
@@ -6,18 +6,27 @@ $(document).ready(function() {
   $('#diagnostics').html(diagnostics);
 
   // lock in screen size to device display size
-  $('<body>').css({
+  $('#container').css({
     height: screen.height,
     width: screen.width,
+    overlow: 'hidden',
   });
+
+  var ITEM_WIDTH = $('.item').css('width');
+  var ITEM_HEIGHT = $('.item').css('height');
 
   // target elements with the "draggable" class
   interact('.draggable')
     .draggable({
       // enable inertial throwing
       inertia: true,
+      restrict: {
+        restriction: 'parent',
+        elementRect: { top: (-1 * ITEM_HEIGHT), left: (- 1* ITEM_WIDTH), bottom: ITEM_HEIGHT, right: ITEM_WIDTH }
+      },
       // enable autoScroll
       autoScroll: true,
+      snap: false,
 
       // call this function on every dragmove event
       onmove: dragMoveListener,
