099191039cec831fb75cc95c2fab1fa17dc345c2
Devesh Dayal <devesh95@gmail.com>
2016-07-10 10:10:11 -0700

    orientation

--- public/js/main.js
+++ public/js/main.js
@@ -6,16 +6,42 @@ $(document).ready(function() {
   $('#diagnostics').html(diagnostics);
 
   // lock in screen size to device display size
-  $('body').css({
-    height: screen.height,
-    width: screen.width,
-    overlow: 'hidden',
-  });
-  $('#container').css({
-    height: screen.height,
-    width: screen.width,
-    overlow: 'hidden',
-  });
+  if (window.orientation != 'null') {
+    if (window.orientation == 'portrait') {
+      $('body').css({
+        height: screen.height,
+        width: screen.width,
+        overlow: 'hidden',
+      });
+      $('#container').css({
+        height: screen.height,
+        width: screen.width,
+        overlow: 'hidden',
+      });
+    } else {
+      $('body').css({
+        height: screen.width,
+        width: screen.height,
+        overlow: 'hidden',
+      });
+      $('#container').css({
+        height: screen.width,
+        width: screen.height,
+        overlow: 'hidden',
+      });
+    }
+  } else {
+    $('body').css({
+      height: screen.height,
+      width: screen.width,
+      overlow: 'hidden',
+    });
+    $('#container').css({
+      height: screen.height,
+      width: screen.width,
+      overlow: 'hidden',
+    });
+  }
 
   $('body').on({
     'mousewheel': function(e) {
