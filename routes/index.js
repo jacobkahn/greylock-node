6cf02ba7ff663207230e78c26d74cd1ec41b77e6
Jacob Kahn <jacobkahn1@gmail.com>
2016-07-09 19:06:49 -0700

    Router

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:session_id/:phone_id', function(req, res, next) {
+router.get('/page/:session_id/:phone_id', function(req, res, next) {
   res.render('index', {
   	title: 'Express',
   	phone_id: req.params.phone_id,
